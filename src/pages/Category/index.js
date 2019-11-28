68e6730d64a1bbf1674912fed3afb8f0146fa829
qhuang05 <qhuang05@sina.com>
2019-11-28 19:01:04 +0800

    update

--- src/pages/Category/index.js
+++ src/pages/Category/index.js
@@ -10,29 +10,33 @@ class CategoryPage extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			page: 10,	//tab标签可视个数
 			tabItemHeight: 46,
+			tabsHeight: 'auto', //tab总高度
+			page: 10,	//tab标签可视个数
 		}
+	}
+	componentDidMount() {
+		this.props.getCategory();
+		// let maxHeight = this.contElem.clientHeight;
+		// let contHeight = this.state.tabItemHeight*
+		// let _page = Math.round(contHeight / this.state.tabItemHeight);
+		// this.setState({ page: _page });
 	}
 	renderContent = tab => (
 		<div style={{ backgroundColor: '#fff' }}>
 			<p>Content of {tab.title}</p>
 		</div>
 	)
-	componentDidMount() {
-		this.props.getCategory();
-		let contHeight = this.contElem.clientHeight;
-		let _page = Math.round(contHeight / this.state.tabItemHeight);
-		this.setState({ page: _page });
-	}
 	render() {
 		let { page } = this.state;
 		let { category } = this.props.category;
+		
+		console.log(this.props.category.length);	
 		return (
 			<Layout>
 				{/* <SearchBar /> */}
-				<div style={{height: '90vh'}}>
-					<div ref={el => this.contElem = el}>
+				<div ref={el => this.contElem = el} style={{height: '90vh'}}>
+					<div style={{height: this.state.tabsHeight}}>
 						<Tabs
 							tabs={category}
 							tabBarPosition="left"
